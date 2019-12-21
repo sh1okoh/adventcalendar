4b04de44dc82d6b2ae3a674dd17bd19388e71383
塩沢　光貴 <ko.shiozawa@a1706-084.opt-sys.local>
2019-12-22 00:35:49 +0900

    リファクタとか

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 
-console.clear();
 
 const Title = ({todoCount}) => {
   return (
     <div>
-       <div>
-          <h1>to-do ({todoCount})</h1>
-       </div>
+      {
+          todoCount > 0 ? <h1>{todoCount}つのタスクがあります</h1> :
+          <h1>タスクがありません</h1>
+      }
     </div>
   );
 }
 
-const TodoForm = ({addTodo}) => {
+const TodoForm = ({addTodo, isEdit}) => {
   let input;
   return (
     <form onSubmit={(e) => {
@@ -21,46 +21,59 @@ const TodoForm = ({addTodo}) => {
         addTodo(input.value);
         input.value = '';
       }}>
-      <input className="form-control col-md-12" ref={node => {
+        <br />
+      {isEdit ? "aaa" : <input className="form-control col-md-12" ref={node => {
         input = node;
-      }} />
+      }} />}
+      <br />
       <br />
     </form>
   );
 };
 
-const Todo = ({todo, remove, update}) => {
+const Todo = ({todo, remove, changeForm, isEdit}) => {
   return (
-    <li>
-      <a href="#" className="list-group-item">{todo.contents}</a>
-      <button onClick={() => {remove(todo.id)}}>削除</button>
-      
-      <button onClick={() => {update(todo.id)}}>編集する</button>
+    <li style={{listStyle: "none"}}>
+      {
+        isEdit ?
+        <>
+        <input className="form-control col-md-12" value={todo.contents} onChange={(event)=> console.log(event)}/>
+        <button onClick={() => {}}>更新する</button>
+        
+        </>
+        :
+        <>
+        <a href="#" className="list-group-item">{todo.contents}</a>
+        <button onClick={() => {remove(todo.id)}}>削除</button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        {/* <button onClick={() => {changeForm(todo.id)}}>編集する</button> */}
+        </>
+      }
     </li>
   );
 }
 
-const TodoList = ({todos, remove, update, isChangeForm}) => {
+const TodoList = ({todos, remove, changeForm, isEdit}) => {
   const todoNode = todos.map((todo) => {
-    return (<Todo todo={todo} key={todo.id} remove={remove} update={update} isChangeForm={}/>)
+    return (
+        <Todo todo={todo} key={todo.id} remove={remove} changeForm={changeForm} isEdit={isEdit}/>
+    )
   });
   return (<div className="list-group" style={{marginTop:'30px'}}>{todoNode}</div>);
 }
 
-// Contaner Component
-// Todo Id
 window.id = 0;
 class TodoApp extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      data: [],
-      updateBtn: false
+      data: []
     }
     this.apiUrl = 'http://localhost:3000/api/todos/'
   }
 
   componentDidMount(){
+    console.log("2");
     axios.get(this.apiUrl)
       .then((res) => {
         this.setState({ data: res.data });
@@ -94,14 +107,11 @@ class TodoApp extends React.Component{
       })
   }
 
-  handleOnChangeForm() {
-    this.setState({...this.state, updateBtn: true})
+  handleOnChangeForm(id) {
+    this.setState({...this.state, isEditForm: !this.isEditForm})
   }
 
   // handleUpdate(id) {
-  //   const reminder = this.state.data.filter((todo) => {
-  //     if (todo.id !== id) return todo;
-  //   })
   //   axios.put(this.apiUrl+'/'+id, todo.text)
   //   .then((res) => {
   //     this.setState({data: reminder})
@@ -109,7 +119,7 @@ class TodoApp extends React.Component{
   // }
 
   render(){
-
+    console.log("1");
     return (
       <div>
         <Title todoCount={this.state.data.length}/>
@@ -117,8 +127,9 @@ class TodoApp extends React.Component{
         <TodoList
           todos={this.state.data}
           remove={this.handleRemove.bind(this)}
-          update={this.handleUpdate.bind(this)}
-          isChangeForm={}
+          changeForm={this.handleOnChangeForm.bind(this)}
+          isEdit={this.state.isEditForm}
+          // isChangeForm={this.handleOnChangeForm.bind(this)}
         />
       </div>
     );
